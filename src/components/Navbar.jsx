ed0a88b217014e539e1afde3d3fe5b3699450e38
NARENDRAKANUMURI <119294166+NARENDRAKANUMURI@users.noreply.github.com>
2026-02-18 15:44:44 +0530

    Add title to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
       animate={{ y: 0 }}
       className="bg-black text-white flex justify-between items-center px-8 py-4 shadow-lg sticky top-0 z-50"
     >
+       <h1 className="text-xl font-bold">Narendra</h1>
 
       <div className="space-x-6">
         <Link to="/">Home</Link>
